f81fa6f5025291b35269312e6b940eb8ef1bc627
Sungjoon Yoon <hughsjyoon@gmail.com>
2013-11-30 15:12:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hughsjyoon::Application.config.secret_key_base = 'e443f70f8fe10dfbf62445c555689a3a5bab81725d3c9cd77061c86963c96277b63f1ade76ccecb67397ccb03921d27cd1ec5043b67a5294d089c132b63aba9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hughsjyoon::Application.config.session_store :cookie_store, key: '_hughsjyoon_session'
